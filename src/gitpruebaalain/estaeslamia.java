6738150b2e668a4a1df9385d063e6700ab201b92
IN1DM3B_24 <IN1DM3B_24@HZ317808.H015112.NET>
2019-01-10 14:22:41 +0100

    hola otra prueba segunda

--- src/gitpruebaalain/estaeslamia.java
+++ src/gitpruebaalain/estaeslamia.java
@@ -1,10 +1,12 @@
 package gitpruebaalain;
 
+import javax.swing.SingleSelectionModel;
+
 public class estaeslamia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("dejar de hablar de pokemon");
 	}
 
 }
